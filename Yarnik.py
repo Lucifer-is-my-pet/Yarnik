e06ea03cf9db3db5a561403911c76aebdb822538
Lucifer-is-my-pet <dianka-piratka@list.ru>
2014-10-22 00:32:46 +0600

    Йа словарчег

--- Yarnik.py
+++ Yarnik.py
@@ -10,32 +10,15 @@ def distance(first_point, second_point):
 
 
 # возвращает [индекс вершины, вес ребра до неё]
-def searching_for_the_most_cheap(list_of_costs):
-    if len(list_of_costs) == 5:
-        print("costs", list_of_costs)
-    resultind = 0
-    if list_of_costs[resultind] != 0:
-        mini = list_of_costs[resultind]
-    else:
-        mini = list_of_costs[resultind + 1]
-        resultind += 1
-    for im in range(len(list_of_costs)):
-        if (list_of_costs[im] < mini) and (list_of_costs[im] != 0) and (im not in selectedVertices):
-            print(im, "element =", list_of_costs[im], "is not in", selectedVertices)
-            mini = list_of_costs[im]
+def searching_for_the_cheapest(dict_of_costs):
+    resultind = list(dict_of_costs.keys())[0]
+    mini = dict_of_costs[resultind]
+    for im in list(dict_of_costs.keys()):
+        if dict_of_costs[im] < mini:
+            mini = dict_of_costs[im]
             resultind = im
-        elif (list_of_costs[im] == mini) and (list_of_costs[im] != 0) and (im not in selectedVertices):
-            mini = list_of_costs[im]
-            resultind = im
-    print("The most cheap is", mini)
-    return [resultind, mini]
+    return resultind
 
-
-# возвращает строку, в которой находится элемент с известным столбцовым индексом
-def searching_for_damn_index(column_ind, its_value):
-    for ind in range(numOfStrings):
-        if adjacencyMatrix[ind][column_ind] == its_value:
-            return ind
 
 f = open('in.txt', 'r')
 numOfStrings = int(f.readline())
@@ -47,65 +30,49 @@ for i in range(numOfStrings):
 f.close()
 
 # для работы
-adjacencyMatrix = [[0 for x in range(numOfStrings)] for y in range(numOfStrings)]
+dictOfCosts = {}
+
+# выбираем начальную точку
+selectedVertices = [0]
 # для вывода в файл
 adjacencyList = [[] for y in range(numOfStrings)]
 
-for n in range(numOfStrings):
-    for p in range(numOfStrings):
-        if n == p:
-            adjacencyMatrix[n][p] = 0
-        elif n > p:
-            adjacencyMatrix[n][p] = adjacencyMatrix[p][n]
-        else:
-            adjacencyMatrix[n][p] = distance(dictOfCoordinates[n], dictOfCoordinates[p])
-print("adjacencyMatrix", adjacencyMatrix)
-# выбираем начальную точку
-selectedVertices = [0]
-selectedVertices[0] = 0
+for v in range(1, numOfStrings):
+    dictOfCosts[v] = distance(dictOfCoordinates[0], dictOfCoordinates[v])
+print("dictOfCosts", dictOfCosts)
 
 # первая итерация - запускаем цикл поиска самого дешёвого ребра, инцидентного ей.
 # для этого сравниваем числа в строке, соответствующей номеру вершины
-first_iteration_result = searching_for_the_most_cheap(adjacencyMatrix[0])[0]
+first_iteration_result = searching_for_the_cheapest(dictOfCosts)
 selectedVertices.append(first_iteration_result)
 adjacencyList[0].append(first_iteration_result)
 adjacencyList[first_iteration_result].append(0)
+dictOfCosts.pop(first_iteration_result)
 
 # далее повторяем процедуру, но уже для двух точек: начальной и добавленной (и так далее)
 # условие остановки: len(selectedVertices) = numOfStrings
 # глобальный цикл
-for n in range(numOfStrings - 1):
+for n in range(numOfStrings - 2):
     print("------")
+    for m in list(dictOfCosts.keys()):
+        tempCost = distance(dictOfCoordinates[selectedVertices[-1]], dictOfCoordinates[m])
+        if tempCost < dictOfCosts[m]:
+            dictOfCosts[m] = tempCost
+    print("dictOfCosts before", dictOfCosts)
+
+    # ищем наименьшее из посчитанных расстояний
+    newVertice = searching_for_the_cheapest(dictOfCosts)
+    selectedVertices.append(newVertice)
+    adjacencyList[selectedVertices[-1]].append(newVertice)
+    adjacencyList[newVertice].append(selectedVertices[-1])
+    dictOfCosts.pop(newVertice)
+    print("dictOfCosts after", dictOfCosts)
     print(n + 2, "step, selected vertices:", selectedVertices)
-    dictOfTempCostsAndInd = {}
-    # тут мы получаем m=len(selectedVertices) списков вида "n" : [result, min] (ключ - вершина из selectedVertices)
-    # нам нужно выбрать наименьший min и добавить к selectedVertices соответствующий ему result
-    # но всё не так-то просто. нужно помнить, для какой из вершин мы нашли
-    # каждый из списков [result, min], среди которых - определивший результат
-    for m in selectedVertices:
-        dictOfTempCostsAndInd[m] = searching_for_the_most_cheap(adjacencyMatrix[m])
-    print("dictOfTempCostsAndInd", dictOfTempCostsAndInd)
-    listOfTempCosts = [0 for x in range(len(selectedVertices))]
-    listOfTempIndexes = [0 for x in range(len(selectedVertices))]
-    # отдельно выделяем стоимости
-    d = 0
-    for p in selectedVertices:
-        listOfTempCosts[d] = dictOfTempCostsAndInd[p][1]
-        listOfTempIndexes[d] = dictOfTempCostsAndInd[p][0]
-        d += 1
-    tempList = searching_for_the_most_cheap(listOfTempCosts)
-    # индекс искомой стоимости в промежуточном массиве
-    tempInd = listOfTempCosts.index(tempList[1])
-    # индекс искомой стоимости в изначальном списке
-    desiredInd = listOfTempIndexes[tempInd]
-    selectedVertices.append(desiredInd)
-    initialIndex = searching_for_damn_index(desiredInd, tempList[1])
-    adjacencyList[initialIndex].append(desiredInd)
-    adjacencyList[desiredInd].append(initialIndex)
+    for index in adjacencyList:
+        print(index)
 
-
-for index in adjacencyList:
-    print(index)
+# for index in adjacencyList:
+#     print(index)
 # создаём список списков, длиной = numOfStrings, элемент - список смежных данной вершин
 # f = open('out.txt', 'w')
 # TODO
